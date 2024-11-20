72062c87833d143ec0b127c52ac49d3c998fc51d
Tiago Behenck <tiago.behenck.santos@gmail.com>
2024-11-20 16:48:25 -0300

    setup tests with vitest, create a example of tests

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
+import Link from 'next/link'
+ 
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <div>
+      <h1>Home</h1>
+      <Link href="/about">About</Link>
+    </div>
+  )
 }
