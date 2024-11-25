440e9f111f3417c3484381781027e50a6540e9fd
Tiago Behenck <tiago.behenck.santos@gmail.com>
2024-11-25 11:42:32 -0300

    add  `baseUrl` and `path`

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,9 +1,9 @@
+import database from 'infra/database'
 import { NextResponse } from 'next/server'
-import database from '../../../../infra/database'
+
 
 export async function GET() {
   const result = await database.query('SELECT 1 + 1;')
 
-  console.log(result)
   return NextResponse.json({ message: 'alunos do curso.dev são pessoas acima da média!' }, { status: 200 })
 }
