2468d82e10c85232504ae6df662d2b28ed0998ea
Tiago Behenck <tiago.behenck.santos@gmail.com>
2024-11-25 11:12:29 -0300

    using `zod` to validate environment variables

--- infra/database.ts
+++ infra/database.ts
@@ -1,12 +1,14 @@
 import { Client } from 'pg'
 
+import { env } from '../env'
+
 async function query(queryObject) {
   const client = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: Number(process.env.POSTGRES_PORT),
-    user:  process.env.POSTGRES_USER,
-    database: process.env.POSTGRES_DB,
-    password: process.env.POSTGRES_PASSWORD,
+    host: env.POSTGRES_HOST,
+    port: env.POSTGRES_PORT,
+    user: env.POSTGRES_USER,
+    database: env.POSTGRES_DB,
+    password: env.POSTGRES_PASSWORD,
   })
   await client.connect()
   const result = await client.query(queryObject)
