92045b67165805aa7922525f67a9b52232ce985d
Tiago Behenck <tiago.behenck.santos@gmail.com>
2024-11-25 10:59:00 -0300

    add `database.ts` and `.env` files

--- app/api/v1/status/route.ts
+++ app/api/v1/status/route.ts
@@ -1,5 +1,9 @@
 import { NextResponse } from 'next/server'
+import database from '../../../../infra/database'
 
-export  async function GET() {
+export async function GET() {
+  const result = await database.query('SELECT 1 + 1;')
+
+  console.log(result)
   return NextResponse.json({ message: 'alunos do curso.dev são pessoas acima da média!' }, { status: 200 })
 }
