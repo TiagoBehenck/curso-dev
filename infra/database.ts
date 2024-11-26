b34cd8b2b780b2591ffdce237ae777f60ffa3287
Tiago Behenck <tiago.behenck.santos@gmail.com>
2024-11-26 16:36:51 -0300

    don't use `env` by zod, just to test

--- infra/database.ts
+++ infra/database.ts
@@ -4,11 +4,11 @@ import { env } from '../env'
 
 async function query(queryObject) {
   const client = new Client({
-    host: env.POSTGRES_HOST,
-    port: env.POSTGRES_PORT,
-    user: env.POSTGRES_USER,
-    database: env.POSTGRES_DB,
-    password: env.POSTGRES_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: Number(process.env.POSTGRES_PORT),
+    user: process.env.POSTGRES_USER,
+    database: process.env.POSTGRES_DB,
+    password: process.env.POSTGRES_PASSWORD,
   })
 
   try {
