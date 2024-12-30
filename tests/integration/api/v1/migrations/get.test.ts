1511b7c23ee142f1f5fe7a70bca77290565702d1
Tiago Behenck <tiago.behenck.santos@gmail.com>
2024-12-30 17:14:34 -0300

    test: rename test

--- tests/integration/api/v1/migrations/get.test.ts
+++ tests/integration/api/v1/migrations/get.test.ts
@@ -2,7 +2,7 @@ import { describe, expect, test } from 'vitest'
 
 describe('GET /api/v1/migrations', () => {
   describe('Anonymous user', () => {
-    test('Running pending migrations', async () => {
+    test('Retrieving pending migrations', async () => {
       const response = await fetch('http://localhost:3000/api/v1/migrations')
 
       expect(response.status).toBe(200)
